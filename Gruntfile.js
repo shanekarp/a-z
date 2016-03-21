b575ab0fab728999dcef270abef2c23e7a9b449c
Johnathan Howard <johnathan.howard.jr@saic.com>
2016-03-21 08:42:23 -0400

    added minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,17 +16,37 @@ module.exports = function(grunt) {
         browserify: {
             dist: {
                 files: {
-                    './e2/scripts/rv7/organization/index.js': ['_js/globals/header.js']
+                    './e2/scripts/rv7/organization/<%= pkg.name %>.js': ['_js/globals/header.js']
                 },
                 options: {
                     transform: ['babelify']
                 }
             }
-        }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                mangle: true,
+                compress: true,
+                beautify: false
+            },
+            build: {
+                files: [{
+                    src: [
+                        'e2/scripts/rv7/organization/<%= pkg.name %>.js'
+                    ],
+                    dest: 'e2/scripts/rv7/organization/<%= pkg.name %>.min.js'
+                }]
+            }
+        },
     });
 
     grunt.loadNpmTasks('grunt-browserify');
 
     grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('production', ['sass', 'browserify', 'uglify']);
 
 };
